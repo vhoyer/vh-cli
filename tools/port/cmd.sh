b9f5daf346602a8a82ea36fca1c20a7741331a0a
Vinícius Hoyer <contact@vhoyer.dev>
2024-02-20 00:16:40 -0300

    :sparkles: [port] allow port tool to be used in $() expressions

--- tools/port/cmd.sh
+++ tools/port/cmd.sh
@@ -3,16 +3,24 @@
 
 PRGNAME="$(basename "$(dirname "$0")")"
 
-HELP_MSG="Usage: vh $PRGNAME [--help|-h]
+HELP_MSG="Usage: vh $PRGNAME [options]
 
 Generates a random port number between 10000 and 65535. Copies the port number
-to the clipboard."
+to the clipboard.
+
+Options:
+  --help, -h  Show this help message and exit
+  --copy, -c  Copy the port number to the clipboard
+"
 
 while [ $# -gt 0 ]; do
   case "$1" in
     --help|-h)
       echo "$HELP_MSG"
       exit 0
+      ;;
+    --copy|-c)
+      COPY=1
       ;;
     *)
       ;;
@@ -21,6 +29,10 @@ while [ $# -gt 0 ]; do
 done
 
 PORT=$(shuf -i 10000-65535 -n 1)
-echo "$PORT" | xclip -selection clipboard
-echo "Port: $PORT"
-echo "Port copied to clipboard."
+
+if [ -n "$COPY" ]; then
+  echo "$PORT" | xclip -selection clipboard
+  echo "Port copied to clipboard."
+fi
+
+echo "$PORT"
